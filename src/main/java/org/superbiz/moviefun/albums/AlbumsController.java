b405395024bb7d3f370d1ab02d5808ce569ff764
Vladimir Nalitkin <vladimir.nalitkin@gmail.com>
2017-05-10 16:59:40 -0400

    save blob to blobstore

--- src/main/java/org/superbiz/moviefun/albums/AlbumsController.java
+++ src/main/java/org/superbiz/moviefun/albums/AlbumsController.java
@@ -1,23 +1,92 @@
 package org.superbiz.moviefun.albums;
 
+import org.apache.tika.io.IOUtils;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.superbiz.moviefun.blobstore.Blob;
+import org.superbiz.moviefun.blobstore.BlobStore;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.util.Map;
+import java.util.Optional;
+
+import static java.lang.String.format;
+import static org.springframework.http.MediaType.IMAGE_JPEG_VALUE;
 
 @Controller
+@RequestMapping("/albums")
 public class AlbumsController {
 
     private final AlbumsBean albumsBean;
+    private final BlobStore blobStore;
 
-    public AlbumsController(AlbumsBean albumsBean) {
+    public AlbumsController(AlbumsBean albumsBean, BlobStore blobStore) {
         this.albumsBean = albumsBean;
+        this.blobStore = blobStore;
     }
 
 
-    @GetMapping("/albums")
+    @GetMapping
     public String index(Map<String, Object> model) {
         model.put("albums", albumsBean.getAlbums());
         return "albums";
+    }
+
+    @GetMapping("/{albumId}")
+    public String details(@PathVariable long albumId, Map<String, Object> model) {
+        model.put("album", albumsBean.find(albumId));
+        return "albumDetails";
+    }
+
+    @PostMapping("/{albumId}/cover")
+    public String uploadCover(@PathVariable long albumId, @RequestParam("file") MultipartFile uploadedFile) throws IOException {
+        if (uploadedFile.getSize() > 0) {
+            Blob coverBlob = new Blob(
+                getCoverBlobName(albumId),
+                uploadedFile.getInputStream(),
+                uploadedFile.getContentType()
+            );
+
+            blobStore.put(coverBlob);
+        }
+
+        return format("redirect:/albums/%d", albumId);
+    }
+
+    @GetMapping("/{albumId}/cover")
+    public HttpEntity<byte[]> getCover(@PathVariable long albumId) throws IOException, URISyntaxException {
+        Optional<Blob> maybeCoverBlob = blobStore.get(getCoverBlobName(albumId));
+        Blob coverBlob = maybeCoverBlob.orElseGet(this::buildDefaultCoverBlob);
+
+        byte[] imageBytes = IOUtils.toByteArray(coverBlob.inputStream);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType(coverBlob.contentType));
+        headers.setContentLength(imageBytes.length);
+
+        return new HttpEntity<>(imageBytes, headers);
+    }
+
+    @DeleteMapping("/covers")
+    public String deleteCovers() {
+        blobStore.deleteAll();
+        return "redirect:/albums";
+    }
+
+    private Blob buildDefaultCoverBlob() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream input = classLoader.getResourceAsStream("default-cover.jpg");
+
+        return new Blob("default-cover", input, IMAGE_JPEG_VALUE);
+    }
+
+    private String getCoverBlobName(@PathVariable long albumId) {
+        return format("covers/%d", albumId);
     }
 }
